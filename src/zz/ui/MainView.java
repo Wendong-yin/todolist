2193ad55a5a8732b81c4040d96a31cd73143d161
zhaozheid <zhaozheid@gmail.com>
2018-05-06 22:25:44 -0400

    view add and refresh

--- src/zz/ui/MainView.java
+++ src/zz/ui/MainView.java
@@ -9,6 +9,8 @@ import zz.model.TodoList;
 import zz.service.TodoListService;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
 public class MainView extends View {
@@ -27,6 +29,9 @@ public class MainView extends View {
     private static final int windowPadding = 24;
 
     private Label titleLabel;
+    private Button addButton;
+    private TextField textField;
+    private View containerView;
 
     @Override
     protected void initSubviews() {
@@ -44,15 +49,31 @@ public class MainView extends View {
         inputView.setLocation(0, this.getHeight() - inputView.getHeight() - windowPadding);
         this.add(inputView);
 
-        Button addButton = new Button("添加");
+        addButton = new Button("添加");
         addButton.setSize(100, cellHeight);
         addButton.setLocation(this.getWidth() - paddingSmall - addButton.getWidth(), paddingSmall);
         inputView.add(addButton);
 
-        TextField textField = new TextField();
+        textField = new TextField();
         textField.setSize(addButton.getX() - 2 * paddingSmall, cellHeight);
         textField.setLocation(paddingSmall, paddingSmall);
         inputView.add(textField);
+    }
+
+    @Override
+    protected void initEvents() {
+        super.initEvents();
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TodoItem todoItem = new TodoItem(textField.getText());
+                todoListService.add(todoItem);
+                textField.setText("");
+
+                dataToView();
+            }
+        });
     }
 
     @Override
@@ -66,15 +87,22 @@ public class MainView extends View {
 
         titleLabel.setText(todoList.getTitle());
 
-        int y = titleLabel.getY() + titleLabel.getHeight() + paddingNormal;
+        if (containerView != null) {
+            this.remove(containerView);
+        }
+        containerView = new View();
+        containerView.setLocation(0, titleLabel.getY() + titleLabel.getHeight() + paddingNormal);
+        containerView.setSize(this.getWidth(), this.getHeight() - windowPadding - containerView.getY() - panelHeight);
+        this.add(containerView);
 
+        int y = 0;
         LinkedList<TodoItem> items = todoList.getItems();
         for (TodoItem item : items) {
             Label label = new Label();
             label.setText(item.getText());
             label.setLocation(paddingNormal, y);
             label.setSize(this.getHeight() - 2 * paddingNormal, cellHeight);
-            this.add(label);
+            containerView.add(label);
             y += label.getHeight() + paddingNormal;
         }
     }

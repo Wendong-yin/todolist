11dbd18e59d63936fbaef4a4f1d888ee0c77cbff
wendong <hi.yinwendong@gmail.com>
2018-06-17 23:00:14 -0400

    添加用户添加SQL操作

--- src/main/java/zz/server/persistence/UserPersistence.java
+++ src/main/java/zz/server/persistence/UserPersistence.java
@@ -13,7 +13,7 @@ public class UserPersistence {
         try{
             // ❤️ 我们要从 query 返回的字符串 不断提取
             user.setId(resultSet.getLong("id"));
-            user.setPassword(resultSet.getString("username"));
+            user.setUsername(resultSet.getString("username"));
             user.setPassword(resultSet.getString("password"));
         }catch (SQLException e){
             e.printStackTrace();
@@ -23,7 +23,32 @@ public class UserPersistence {
 
     public User getByUsername (User user){
 
-        String query = "SELECT * FROM \"user\" where username = '" + user.getUsername() +"';";
+        //String query = "SELECT * FROM \"user\" where username = '" + user.getUsername() +"';";
+
+        // ❤️ 这样写搜索更加优雅
+        String query = "SELECT * FROM \"user\" where username = '%s';";
+        query = String.format(query,user.getUsername());
+
+        Connection connection =  Database.newConnection();
+        try {
+            Statement statement = connection.createStatement();
+            // ❤️ sql 返回的结果是一个 result 对象
+            ResultSet resultSet = statement.executeQuery(query);
+
+            if(!resultSet.next()){
+                return null;
+            }
+            return UserPersistence.fromResult(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public User create (User user){
+        // ❤️ 这样写搜索更加优雅
+        String query = "INSERT INTO \"user\" VALUES (DEFAULT ,'%s','%s') RETURNING  *;\n";
+        query = String.format(query,user.getUsername(),user.getPassword());
 
         Connection connection =  Database.newConnection();
         try {
@@ -47,6 +72,12 @@ public class UserPersistence {
         user.setUsername("wendong");
         user = userPersistence.getByUsername(user);
         System.out.println(user);
+
+        User user1 = new User();
+        user1.setUsername("cai22");
+        user1.setPassword("123556");
+        User user2 = userPersistence.create(user1);
+        System.out.println(user2);
     }
 
 }
